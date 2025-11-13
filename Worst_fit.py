355497b4ac9fc0d934dc8e91325f02593f3c31dd
nehapatil06 <nehapatil.1456@gmail.com>
2025-11-13 14:44:01 +0530

    Update Worst_fit.py

--- Worst_fit.py
+++ Worst_fit.py
@@ -1,18 +1,7 @@
 def worstFit(blockSize, m, processSize, n): 
-	
-	# Stores block id of the block 
-	# allocated to a process 
-	
-	# Initially no block is assigned 
-	# to any process 
 	allocation = [-1] * n 
 	
-	# pick each process and find suitable blocks 
-	# according to its size ad assign to it 
 	for i in range(n): 
-		
-		# Find the best fit block for 
-		# current process 
 		wstIdx = -1
 		for j in range(m): 
 			if blockSize[j] >= processSize[i]: 
@@ -21,20 +10,13 @@ def worstFit(blockSize, m, processSize, n):
 				elif blockSize[wstIdx] < blockSize[j]: 
 					wstIdx = j 
 
-		# If we could find a block for 
-		# current process 
-		if wstIdx != -1: 
-			
-			# allocate block j to p[i] process 
+		if wstIdx != -1:
 			allocation[i] = wstIdx 
-
-			# Reduce available memory in this block. 
 			blockSize[wstIdx] -= processSize[i] 
 
 	print("Process No. Process Size Block no.") 
 	for i in range(n): 
-		print(i + 1, "		 ", 
-			processSize[i], end = "	 ") 
+		print(i + 1, "		 ",processSize[i], end = "	 ") 
 		if allocation[i] != -1: 
 			print(allocation[i] + 1) 
 		else: 
@@ -45,6 +27,4 @@ if __name__ == '__main__':
 	processSize = [212, 417, 112, 426] 
 	m = len(blockSize) 
 	n = len(processSize) 
-
 	worstFit(blockSize, m, processSize, n) 
-
